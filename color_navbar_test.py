7184ff4d6803db98c37e0b881cb0afcaa9558ec3
degoldschmidt <goldschmidt.de@gmail.com>
2018-03-26 15:33:48 +0200

    new stuff

--- color_navbar_test.py
+++ color_navbar_test.py
@@ -6,12 +6,11 @@ import threading
 import webbrowser
 import json
 import os
+import matplotlib
+matplotlib.use('tkAgg')
+import matplotlib.pyplot as plt
 
-# My modules
-#import twitch_data as twitch
-#import color
-#import font
-#import style
+from tkinter_png import *
 
 def dit(*args):
     pass
@@ -21,30 +20,24 @@ dark_purple = '#8700e6'
 light_purple = '#bf77ff'
 blue = '#0044ff'
 
+def get_images(paths):
+
+    return [PhotoImage(file=path) for path in paths]##[ImageTk.PhotoImage(img) for img in pil_img]
+
 class TestApp(ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
-        init_styles()
-        #self.parent = parent
-        #self.init_frames()
-        #self.grid_frames()
-        ttk.Label(parent, text = 'Experiments', style='TLabel').grid(row=0, column=0)
-
-class MainApplication(ttk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        ttk.Frame.__init__(self, parent, *args, **kwargs)
-        self.s = None
         init_styles()
         self.parent = parent
         self.init_frames()
         self.grid_frames()
 
     def init_frames(self):
-        self.navbar = Navbar(self)
-        experiments = Experiments(self)
-        videos = Videos(self)
-        sessions = Sessions(self)
-        datasets = Datasets(self)
+        self.navbar = ColorNavBar(self)
+        experiments = EmptyFrame(self)
+        videos = EmptyFrame(self)
+        sessions = EmptyFrame(self)
+        datasets = EmptyFrame(self)
         self.sections = [experiments, videos, sessions, datasets]
 
     def grid_frames(self):
@@ -59,28 +52,36 @@ class MainApplication(ttk.Frame):
             section.grid_remove()
         self.sections[index].grid()
 
-class Navbar(ttk.Frame):
+class ColorNavBar(ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.selection = 0
+
         base_icon='icon'
-        self.sections = [[ImageTk.PhotoImage(Image.open(os.path.join(base_icon, 'experiments_tiny.png'))), 'Experiments'], [ImageTk.PhotoImage(Image.open(os.path.join(base_icon, 'videos.png'))), 'Videos'], [ImageTk.PhotoImage(Image.open(os.path.join(base_icon, 'sessions.png'))), 'Sessions'], [ImageTk.PhotoImage(Image.open(os.path.join(base_icon, 'datasets.png'))), 'Datasets']]
+        section_names = ['Experiments', 'Videos', 'Sessions', 'Datasets'] #icon.jpeg'
+        image_files = [os.path.join(base_icon, name.lower()+'.png') for name in section_names]
+        images = get_images(image_files)
+        self.sections = [(image, section) for image, section in zip(images, section_names)]
         self.frames = []
+        self.list_img = section_names
         self.selection = 0
         self.init_widgets()
-        self.change_style(self.frames[self.selection], 'light.TFrame', 'light.TLabel')
+        #self.change_style(self.frames[self.selection], 'light.TFrame', 'light.TLabel')
         self.grid_widgets()
 
     def init_widgets(self):
         # Frameception
         for index, section in enumerate(self.sections):
             frame = ttk.Frame(self, style = 'dark.TFrame')
-            li = ttk.Label(frame, image = section[0], style = 'dark.TLabel')
+            print(section)
+            self.list_img[index] = ttk.Label(frame, image = section[0], style = 'dark.TLabel')
+            self.list_img[index].image = section[0] # keep a reference!
             lt = ttk.Label(frame, text = section[1], style = 'dark.TLabel')
-            frame.bind('<Enter>', self.enter)
-            frame.bind('<Leave>', self.leave)
-            #li.bind('<Button-1>', self.click)
-            #lt.bind('<Button-1>', self.click)
+            #frame.bind('<Enter>', self.enter)
+            #frame.bind('<Leave>', self.leave)
+            self.list_img[index].bind('<Button-1>', self.click)
+            lt.bind('<Button-1>', self.click)
             frame.bind('<Button-1>', self.click)
             self.frames.append(frame)
         self.label_filler = tk.Label(self, text = '', bg = dark_purple, height = 100)
@@ -115,7 +116,36 @@ class Navbar(ttk.Frame):
         for child in widget.winfo_children():
             child.configure(style = style_c)
 
-class Experiments(ttk.Frame):
+class MainApplication(ttk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        ttk.Frame.__init__(self, parent, *args, **kwargs)
+        self.s = None
+        init_styles()
+        self.parent = parent
+        self.init_frames()
+        self.grid_frames()
+
+    def init_frames(self):
+        self.navbar = Navbar(self)
+        experiments = Experiments(self)
+        videos = Videos(self)
+        sessions = Sessions(self)
+        datasets = Datasets(self)
+        self.sections = [experiments, videos, sessions, datasets]
+
+    def grid_frames(self):
+        self.navbar.grid(row = 0, column = 0)
+        for section in self.sections:
+            section.grid(row = 0, column = 1, sticky = (N))
+            section.grid_remove()
+        self.section_changed(self.navbar.selection)
+
+    def section_changed(self, index):
+        for section in self.sections:
+            section.grid_remove()
+        self.sections[index].grid()
+
+class EmptyFrame(ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
@@ -123,51 +153,10 @@ class Experiments(ttk.Frame):
         self.grid_widgets()
 
     def init_widgets(self):
-        self.label = ttk.Label(self, text = 'Experiments')
+        self.label = ttk.Label(self, text = 'Empty')
 
     def grid_widgets(self):
         self.label.grid(row = 0, column = 0, padx = 10, pady = 10)
-
-class Videos(ttk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        ttk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        self.init_widgets()
-        self.grid_widgets()
-
-    def init_widgets(self):
-        self.label = ttk.Label(self, text = 'Videos')
-
-    def grid_widgets(self):
-        self.label.grid(row = 0, column = 0, padx = 10, pady = 10)
-
-class Sessions(ttk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        ttk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        self.init_widgets()
-        self.grid_widgets()
-
-    def init_widgets(self):
-        self.label = ttk.Label(self, text = 'Sessions')
-
-    def grid_widgets(self):
-        self.label.grid(row = 0, column = 0, padx = 10, pady = 10)
-
-class Datasets(ttk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        ttk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        self.parent.s = None #self.import_settings()
-        self.init_widgets()
-        self.grid_widgets()
-
-    def init_widgets(self):
-        pass
-
-    def grid_widgets(self):
-        pass
-
 
 def defocus(event):
     event.widget.master.focus_set()
@@ -175,25 +164,27 @@ def defocus(event):
 def init_styles():
     print('init styles')
     s = ttk.Style()
-    print(s.theme_use())
+    s.theme_use('clam')
+    print(s.theme_names(), s.theme_use())
+
     s.configure('TFrame', background = light_grey)
-    s.configure('TLabel', font = ('Open Sans', 12, 'roman'), background = light_grey)
-    s.configure('TButton', padding = 2, font = ('Open Sans', 10, 'regular'))
+    s.configure('TLabel', font = ('Quicksand', 12, 'roman'), background = light_grey)
+    s.configure('TButton', padding = 2, font = ('Quicksand', 10, 'roman'))
     s.configure('TEntry')
     s.configure('TLabelframe', background = light_grey)
-    s.configure('TCheckbutton', background = light_grey, font = ('Open Sans', 10, 'regular'))
+    s.configure('TCheckbutton', background = light_grey, font = ('Quicksand', 10, 'roman'))
 
-    s.configure('large.TLabel', background = light_grey, font = ('Open Sans', 14, 'regular'))
-    s.configure('medium.TLabel', background = light_grey, font = ('Open Sans', 12, 'regular'))
-    s.configure('small.TLabel', background = light_grey, font = ('Open Sans', 10, 'regular'))
-    s.configure('tiny.TLabel', background = light_grey, font = ('Open Sans', 8, 'regular'))
+    s.configure('large.TLabel', background = light_grey, font = ('Quicksand', 14, 'roman'))
+    s.configure('medium.TLabel', background = light_grey, font = ('Quicksand', 12, 'roman'))
+    s.configure('small.TLabel', background = light_grey, font = ('Quicksand', 10, 'roman'))
+    s.configure('tiny.TLabel', background = light_grey, font = ('Quicksand', 8, 'roman'))
 
-    s.configure('hyper.TLabel', background = light_grey, foreground = blue, cursor = 'hand2', font = ('Open Sans', 10, 'regular'))
+    s.configure('hyper.TLabel', background = light_grey, foreground = blue, cursor = 'hand2', font = ('Quicksand', 10, 'roman'))
 
     s.configure('dark.TFrame', background = dark_purple)
     s.configure('light.TFrame', background = light_purple)
-    s.configure('dark.TLabel', background = dark_purple, font = ('Open Sans', 12, 'regular'))
-    s.configure('light.TLabel', background = light_purple, font = ('Open Sans', 12, 'regular'))
+    s.configure('dark.TLabel', background = dark_purple, font = ('Quicksand', 12, 'roman'))
+    s.configure('light.TLabel', background = light_purple, font = ('Quicksand', 12, 'roman'))
 
 if __name__ == '__main__':
     root = tk.Tk()
--- test_notebook.py
+++ test_notebook.py
@@ -6,12 +6,13 @@ import sys
 print(sys.platform)
 
 root = Tk()
-image = Image.open('icon.png').convert("RGBA")
-image = image.resize((32,32), Image.ANTIALIAS)
-img = ImageTk.PhotoImage(image)
+image = Image.open('icon/icon.png').convert("RGBA")
+image = image.resize((100,100), Image.ANTIALIAS)
+photo = ImageTk.PhotoImage(image)
+photo = PhotoImage(file='icon/datasets.gif')
 if sys.platform == 'darwin':
     s = Style()
-    s.configure('TNotebook', tabposition='nw') 
+    s.configure('TNotebook', tabposition='nw')
     s.configure('TNotebook.Tab', padding=(20, 8, 20, 0))
 
 note = Notebook(root)
@@ -20,6 +21,10 @@ tab1 = Frame(note)
 tab2 = Frame(note)
 tab3 = Frame(note)
 Button(tab1, text='Exit', command=root.destroy).pack(padx=10, pady=10)
+label = Label(root, image=photo)
+label.image = photo # keep a reference!
+label.pack()
+
 
 note.add(tab1, text=    "      Tab One      ")
 note.add(tab2, text =   "      Tab Two      ")
